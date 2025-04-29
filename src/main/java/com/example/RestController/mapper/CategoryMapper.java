23835bf503c7d5501a40fe3dc17ef08203cfad5a
sumanPSTU <ug2202014@cse.pstu.ac.bd>
2025-04-29 08:25:38 +0600

    add RestAPI file

--- src/main/java/com/example/RestController/mapper/CategoryMapper.java
+++ src/main/java/com/example/RestController/mapper/CategoryMapper.java
@@ -1,4 +1,23 @@
 package com.example.RestController.mapper;
 
+import com.example.RestController.dto.CategoryDTO;
+import com.example.RestController.entity.Category;
+
 public class CategoryMapper {
+
+    public static CategoryDTO toCreateDTO(Category category){
+        if (category == null) return null;
+
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setId(category.getId());
+        categoryDTO.setName(category.getName());
+        categoryDTO.setProducts(category.getProducts().stream().map());
+    }
+
+
+    public static Category toCategoryEntity(CategoryDTO categoryDTO){
+        Category category = new Category();
+        category.setName(categoryDTO.getName());
+        return category;
+    }
 }
--- src/main/java/com/example/RestController/services/CategoryServices.java
+++ src/main/java/com/example/RestController/services/CategoryServices.java
@@ -2,13 +2,18 @@ package com.example.RestController.services;
 
 
 import com.example.RestController.dto.CategoryDTO;
+import com.example.RestController.entity.Category;
+import com.example.RestController.mapper.CategoryMapper;
+import com.example.RestController.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryServices {
     // create category
-    public CategoryDTO createCategoryDTO(CategoryDTO categoryDTO){
-        return categoryDTO;
+    public CategoryRepository categoryRepository;
+    public CategoryDTO createCategory(CategoryDTO categoryDTO){
+        Category category = CategoryMapper.toCategoryEntity(categoryDTO);
+        categoryRepository.save(category);
     }
 
     //get all category
